8b55296836392098015c85edda65813706856a85
百小僧 <monk@baisoft.org>
2016-10-19 11:15:40 +0800

    ok

--- Monk/DbStore/DbConfig.cs
+++ Monk/DbStore/DbConfig.cs
@@ -8,36 +8,25 @@ namespace Monk.DbStore
 {
     public partial class DbServices
     {
-        public static string ConnectionString
+        private static string ConnectionString = ConfigurationManager.ConnectionStrings[Keys.ConnectionStringKey].ToString();
+
+        private static Dictionary<string, Func<KeyValueObj>> RowFilter = new Dictionary<string, Func<KeyValueObj>>()
         {
-            get
             {
-                return ConfigurationManager.ConnectionStrings[Keys.ConnectionStringKey].ToString();
-            }
-        }
-        public static Dictionary<string, Func<KeyValueObj>> RowFilter
-        {
-            get
+                "all",()=> {
+                     return new KeyValueObj(){ Key=" Del=@Del and Destroy=@Destroy " , Value=new{ Del=0 , Destroy=0 }};
+                }
+            },
+             {
+                "admin",()=> {
+                     return new KeyValueObj(){ Key=" Destroy=@Destroy " , Value=new{ Destroy=0 }};
+                }
+            },
             {
-                return new Dictionary<string, Func<KeyValueObj>>()
-                {
-                    {
-                        "all",()=> {
-                             return new KeyValueObj(){ Key=" Del=@Del and Destroy=@Destroy " , Value=new{ Del=0 , Destroy=0 }};
-                        }
-                    },
-                     {
-                        "admin",()=> {
-                             return new KeyValueObj(){ Key=" Destroy=@Destroy " , Value=new{ Destroy=0 }};
-                        }
-                    },
-                    {
-                        "root",()=> {
-                             return new KeyValueObj(){ Key="" , Value=new{ }};
-                        }
-                    }
-                };
+                "root",()=> {
+                     return new KeyValueObj(){ Key="" , Value=new{ }};
+                }
             }
-        }
+        };
     }
 }
