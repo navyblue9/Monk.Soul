917d33893fb05d51ebd8a873b6bfe3e7dade5e9c
百小僧 <monk@baisoft.org>
2016-10-20 14:15:17 +0800

    ok

--- Monk/Areas/Backend/Controllers/AuthorizeController.cs
+++ Monk/Areas/Backend/Controllers/AuthorizeController.cs
@@ -1,24 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using SqlSugar;
-using Monk.DbStore;
+using RestSharp;
+using SyntacticSugar;
 using Monk.Models;
 
 namespace Monk.Areas.Backend.Controllers
 {
-    public class AuthorizeController : BaseController
+    public class AuthorizeController : Controller
     {
-        public AuthorizeController(DbServices services) : base(services) { }
-
-        public JsonResult Index()
+        public ActionResult Index()
         {
-            List<Havior> list = new List<Havior>();
-            services.Command((db) =>
-            {
-                list = db.Queryable<Havior>().Where(c => true).ToList();
-            });
-            return Json(list, JsonRequestBehavior.AllowGet);
+            return View();
         }
     }
 }
---
+++ Monk/Areas/Services/Controllers/MemberController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Monk.DbStore;
+
+namespace Monk.Areas.Services.Controllers
+{
+    public class MemberController : BaseController
+    {
+        public MemberController(DbServices services) : base(services) { }
+
+        [HttpPost]
+        public JsonResult Index(int pageIndex, int pageSize)
+        {
+            return Json(new { success = "ok" });
+        }
+    }
+}
