98d46e0605457ceda52c945b856a57d42d07e3a0
百小僧 <monk@baisoft.org>
2016-10-20 20:27:01 +0800

    ok

---
+++ Monk/Areas/Backend/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Monk.Areas.Backend.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
