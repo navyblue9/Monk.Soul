58ddb8e1e5be2f68018dff4bd0c0984a70b11277
百小僧 <monk@baisoft.org>
2016-10-21 18:49:41 +0800

    ok

---
+++ Monk/Areas/Backend/ViewModels/SigninModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Monk.Areas.Backend.ViewModels
+{
+    public class SigninModel
+    {
+        [Display(Name = "会员账号", Description = "账号由3-16位英文字母，数字或中文组成")]
+        public string Account { get; set; }
+
+        [Display(Name = "会员密码", Description = "密码由6-20位英文字母、数字或符号组成")]
+        public string Password { get; set; }
+    }
+}
---
+++ Monk/Utils/RazorExpressions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Monk.Utils
+{
+    public static class RazorExpressions
+    {
+        public static MvcHtmlString Description(this HtmlHelper htmlHelper, string name)
+        {
+            ModelMetadata _modelMetadata = ModelMetadata.FromStringExpression(name, htmlHelper.ViewData);
+            return MvcHtmlString.Create(_modelMetadata.Description);
+        }
+
+        public static MvcHtmlString DescriptionFor<TModel, TResult>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
+        {
+            ModelMetadata _modelMetadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            return MvcHtmlString.Create(_modelMetadata.Description);
+        }
+    }
+}
