58926e751322a1e8ad7a330607d069824d3eb244
百小僧 <monk@baisoft.org>
2016-10-23 18:34:13 +0800

    ok

--- Monk/Areas/Backend/Injections/SysSetInfoInjectionAttribute.cs
+++ Monk/Areas/Backend/Injections/SysSetInfoInjectionAttribute.cs
@@ -16,35 +16,38 @@ namespace Monk.Areas.Backend.Injections
             var area = filterContext.RouteData.DataTokens["area"];
             if (area != null && area.ToString().ToLower() == "backend")
             {
-                CacheManager<SysSet> cm = CacheManager<SysSet>.GetInstance();
-                var sysSetKey = "SysSetInfo_CacheKey";
-                if (cm.Get(sysSetKey) == null)
+                if (filterContext.HttpContext.Session[Keys.SessionKey] != null)
                 {
-                    var sessionModel = SessionHelper.GetSessionInstance<SessionMember>(Keys.SessionKey);
-                    var apiUrl = new UrlHelper(new RequestContext(filterContext.HttpContext, filterContext.RouteData)).Action("Detail", "SysSet", new { area = "Services" });
-                    RESTFul restful = new RESTFul(RequestInfo.Domain, sessionModel.MemberID.ToString(), RESTFul.GetSecretKey(sessionModel.MemberID.ToString(), Keys.Access_Token));
-                    JsonData<SysSet> clientResult = restful.Get<JsonData<SysSet>>(apiUrl);
-                    if (clientResult.status == "y")
+                    CacheManager<SysSet> cm = CacheManager<SysSet>.GetInstance();
+                    var sysSetKey = "SysSetInfo_CacheKey";
+                    if (cm.Get(sysSetKey) == null)
                     {
-                        cm.Add(sysSetKey, clientResult.data);
+                        var sessionModel = SessionHelper.GetSessionInstance<SessionMember>(Keys.SessionKey);
+                        var apiUrl = new UrlHelper(new RequestContext(filterContext.HttpContext, filterContext.RouteData)).Action("Detail", "SysSet", new { area = "Services" });
+                        RESTFul restful = new RESTFul(RequestInfo.Domain, sessionModel.MemberID.ToString(), RESTFul.GetSecretKey(sessionModel.MemberID.ToString(), Keys.Access_Token));
+                        JsonData<SysSet> clientResult = restful.Get<JsonData<SysSet>>(apiUrl);
+                        if (clientResult.status == "y")
+                        {
+                            cm.Add(sysSetKey, clientResult.data);
+                        }
                     }
-                }
-                var sysSetModel = cm.Get(sysSetKey);
-                ActionResult result = filterContext.Result;
-                if (result is ViewResult)
-                {
-                    ViewResult vresult = result as ViewResult;
-                    vresult.ViewData["SysSetInfo"] = sysSetModel;
-                    filterContext.Result = vresult;
-                }
-                else if (result is PartialViewResult)
-                {
-                    PartialViewResult presult = result as PartialViewResult;
-                    presult.ViewData["SysSetInfo"] = sysSetModel;
-                    filterContext.Result = presult;
-                }
-                else
-                {
+                    var sysSetModel = cm.Get(sysSetKey);
+                    ActionResult result = filterContext.Result;
+                    if (result is ViewResult)
+                    {
+                        ViewResult vresult = result as ViewResult;
+                        vresult.ViewData["SysSetInfo"] = sysSetModel;
+                        filterContext.Result = vresult;
+                    }
+                    else if (result is PartialViewResult)
+                    {
+                        PartialViewResult presult = result as PartialViewResult;
+                        presult.ViewData["SysSetInfo"] = sysSetModel;
+                        filterContext.Result = presult;
+                    }
+                    else
+                    {
+                    }
                 }
             }
         }
