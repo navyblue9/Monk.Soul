ab442be611162b9ca894b05641d61c54f60885e0
百小僧 <monk@baisoft.org>
2016-10-23 20:02:56 +0800

    ok

--- Monk/Areas/Backend/Controllers/SysSetController.cs
+++ Monk/Areas/Backend/Controllers/SysSetController.cs
@@ -12,12 +12,25 @@ namespace Monk.Areas.Backend.Controllers
 {
     public class SysSetController : Controller
     {
+        SessionMember sessionModel = SessionHelper.GetSessionInstance<SessionMember>(Keys.SessionKey);
+
+        [HttpGet]
         public ActionResult Detail()
         {
             SysSet model = new SysSet();
-            var sessionModel = SessionHelper.GetSessionInstance<SessionMember>(Keys.SessionKey);
             RESTFul restful = new RESTFul(RequestInfo.Domain, sessionModel.MemberID.ToString(), RESTFul.GetSecretKey(sessionModel.MemberID.ToString(), Keys.Access_Token));
             JsonData<SysSet> clientResult = restful.Get<JsonData<SysSet>>(Url.Action("Detail", "SysSet", new { area = "Services" }));
+            if (clientResult.status == "y") model = clientResult.data;
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Update(Guid? id)
+        {
+            if (id == null) return Content("非法参数");
+            SysSet model = new SysSet();
+            RESTFul restful = new RESTFul(RequestInfo.Domain, sessionModel.MemberID.ToString(), RESTFul.GetSecretKey(sessionModel.MemberID.ToString(), Keys.Access_Token));
+            JsonData<SysSet> clientResult = restful.Get<JsonData<SysSet>>(Url.Action("Detail", "SysSet", new { area = "Services" }), new { setID = id });
             if (clientResult.status == "y") model = clientResult.data;
             return View(model);
         }
