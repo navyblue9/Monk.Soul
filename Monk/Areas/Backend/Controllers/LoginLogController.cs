f622cb1894cf9a55cca268ae0e38bfb3b4b84464
百小僧 <monk@baisoft.org>
2016-10-25 16:32:51 +0800

    ok

---
+++ Monk/Areas/Backend/Controllers/LoginLogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Monk.Areas.Backend.Controllers
+{
+    public class LoginLogController : Controller
+    {
+        [HttpGet]
+        public ActionResult Select()
+        {
+            return View();
+        }
+    }
+}
