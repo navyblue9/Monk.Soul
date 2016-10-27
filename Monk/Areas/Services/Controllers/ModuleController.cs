9a138bfa004d821204a051b4e242ff68371bd9b7
百小僧 <monk@baisoft.org>
2016-10-27 15:34:01 +0800

    ok

---
+++ Monk/Areas/Services/Controllers/ModuleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using AutoMapper;
+using SqlSugar;
+using Monk.DbStore;
+using Monk.Models;
+using Monk.ViewModels;
+using Monk.Utils;
+using System.Collections.Generic;
+namespace Monk.Areas.Services.Controllers
+{
+    public class ModuleController : BaseController
+    {
+        public ModuleController(DbServices services) : base(services) { }
+
+    }
+}
