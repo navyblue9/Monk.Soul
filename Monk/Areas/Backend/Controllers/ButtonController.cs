958c545710b7322e04fc1e589afbd4ad0a5b8785
百小僧 <monk@baisoft.org>
2016-11-02 01:39:30 +0800

    ok

---
+++ Monk/Areas/Backend/Controllers/ButtonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using SqlSugar;
+using AutoMapper;
+using Monk.Models;
+using Monk.Areas.Backend.ViewModels;
+using Monk.DbStore;
+using Monk.Utils;
+
+namespace Monk.Areas.Backend.Controllers
+{
+    public class ButtonController : BaseController
+    {
+        public ButtonController(DbServices services) : base(services) { }
+
+        public ActionResult Select() { return View(); }
+    }
+}
