d654ba99f78fbcc5e0557787c43fa9acfe8d143f
MonkSoul <monk@baisoft.org>
2016-11-07 03:36:54 +0800

    ok

---
+++ Monk/Areas/Backend/Controllers/PermissionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using SqlSugar;
+using AutoMapper;
+using Monk.Models;
+using Monk.DbStore;
+using Monk.Utils;
+using Monk.Filters;
+using Monk.Areas.Backend.ViewModels;
+
+namespace Monk.Areas.Backend.Controllers
+{
+    public class PermissionController : BaseController
+    {
+        public PermissionController(DbServices services) : base(services) { }
+
+        [HttpGet]
+        public ActionResult Group()
+        {
+            return View();
+        }
+    }
+}
