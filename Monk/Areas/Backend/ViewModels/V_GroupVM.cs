f707b1c110f95bd724435b4db94fafb7b5ae5f9a
MonkSoul <monk@baisoft.org>
2016-11-06 16:40:04 +0800

    ok

---
+++ Monk/Areas/Backend/ViewModels/V_GroupVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Monk.Areas.Backend.ViewModels
+{
+    public class V_GroupVM
+    {
+        public Guid GroupID { get; set; }
+        public string Name { get; set; }
+        public string Remark { get; set; }
+        public Guid? ParentID { get; set; }
+        public Boolean Enable { get; set; }
+        public int SerialNo { get; set; }
+        public DateTime? UpdateTime { get; set; }
+        public Boolean Default { get; set; }
+        public Boolean Del { get; set; }
+        public Boolean Destroy { get; set; }
+        public DateTime CreateTime { get; set; }
+        public Guid LogMemberID { get; set; }
+        public string ParentName { get; set; }
+    }
+}
---
+++ Monk/Models/V_Group.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Monk.Models
+{
+    public class V_Group
+    {
+        
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:False 
+        /// </summary>
+        public Guid GroupID {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:False 
+        /// </summary>
+        public string Name {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:True 
+        /// </summary>
+        public string Remark {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:True 
+        /// </summary>
+        public Guid? ParentID {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:False 
+        /// </summary>
+        public Boolean Enable {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:False 
+        /// </summary>
+        public int SerialNo {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:True 
+        /// </summary>
+        public DateTime? UpdateTime {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:False 
+        /// </summary>
+        public Boolean Default {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:False 
+        /// </summary>
+        public Boolean Del {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:False 
+        /// </summary>
+        public Boolean Destroy {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:False 
+        /// </summary>
+        public DateTime CreateTime {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:False 
+        /// </summary>
+        public Guid LogMemberID {get;set;}
+
+        /// <summary>
+        /// Desc:- 
+        /// Default:- 
+        /// Nullable:True 
+        /// </summary>
+        public string ParentName {get;set;}
+
+    }
+}
