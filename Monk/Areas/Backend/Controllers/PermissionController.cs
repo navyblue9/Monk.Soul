807deeba32e822ef0716d5114bd9c0f2102a9ccd
MonkSoul <monk@baisoft.org>
2016-11-07 14:10:55 +0800

    ok

--- Monk/Areas/Backend/Controllers/PermissionController.cs
+++ Monk/Areas/Backend/Controllers/PermissionController.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 using System.Collections.Generic;
 using SqlSugar;
 using AutoMapper;
+using AutoMapper.Configuration;
 using Monk.Models;
 using Monk.DbStore;
 using Monk.Utils;
@@ -16,9 +17,26 @@ namespace Monk.Areas.Backend.Controllers
         public PermissionController(DbServices services) : base(services) { }
 
         [HttpGet]
-        public ActionResult Group()
+        public ActionResult Group() { return View(); }
+
+        [HttpGet]
+        public JsonResult GroupPermission()
         {
-            return View();
+            var clientResult = new JsonData<object>();
+            var cfg = new MapperConfigurationExpression();
+            cfg.CreateMap<V_Module, V_ModuleVM>();
+            cfg.CreateMap<V_Havior, V_HaviorVM>();
+            cfg.CreateMap<V_Button, V_ButtonVM>();
+            services.Command((db) =>
+            {
+                clientResult.SetClientData("y", "获取成功", new
+                {
+                    modules = Mapper.Map<List<V_ModuleVM>>(db.Queryable<V_Module>().Where(c => true).ToList()),
+                    haviors = Mapper.Map<List<V_HaviorVM>>(db.Queryable<V_Havior>().Where(c => true).ToList()),
+                    buttons = Mapper.Map<List<V_ButtonVM>>(db.Queryable<V_Button>().Where(c => true).ToList())
+                });
+            });
+            return Json(clientResult, JsonRequestBehavior.AllowGet);
         }
     }
 }
